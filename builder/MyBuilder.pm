5cd3a5c6d3d8617e65cc74a86169d560996577a6
hideo55 <hide.o.j55@gmail.com>
2013-06-26 02:41:15 +0900

    Add allow_pureperl option to Module::Build

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -8,13 +8,14 @@ sub new {
     my ( $self, %args ) = @_;
     $self->SUPER::new(
         %args,
+        allow_pureperl => 1,
         c_source       => 'xs',
         xs_files       => { './xs/hyperloglog.xs' => './lib/Algorithm/HyperLogLog.xs', },
         add_to_cleanup => [
             'Algorithm-HyperLogLog-*', 'MANIFEST.bak', 'lib/Algorithm/*.o', 'lib/Algorithm/*.h',
             'lib/Algorithm/*.c',       'lib/Algorithm/*.xs',
         ],
-        meta_add => { keywords => [qw/HyperLogLog cardinality/], },
+        meta_add          => { keywords => [qw/HyperLogLog cardinality/], },
         generate_ppport_h => 'lib/Algorithm/ppport.h',
     );
 }
